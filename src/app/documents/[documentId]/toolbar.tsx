49b9ad6828e95fe1b354d69256e13c1f08aab1e6
nihar-2004 <nihar9363@gmail.com>
2025-09-19 11:46:52 +0530

    added more editor functionality

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { cn } from "@/lib/utils";
-import { LucideIcon, PrinterIcon, Redo2Icon, SpellCheckIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Separator } from "@radix-ui/react-context-menu";
 interface ToolbarButtonProps {
   onClick?: () => void;
   isActive?: boolean;
@@ -60,13 +61,71 @@ export const Toolbar = () => {
     ],
     [
         {
+           label: 'Bold',
+           icon: BoldIcon,
+           isActive: editor?.isActive("bold"),
+           onClick: ()=>editor?.chain().focus().toggleBold().run(), 
+        },
+        {
+           label: 'Italic',
+           icon: ItalicIcon,
+           isActive: editor?.isActive("italic"),
+           onClick: ()=>editor?.chain().focus().toggleItalic().run(), 
+        },
+        {
+           label: 'Underline',
+           icon: UnderlineIcon,
+           isActive: editor?.isActive("underline"),
+           onClick: ()=>editor?.chain().focus().toggleUnderline().run(), 
+        },
+    ],
+    [
+        {
+            label: "Comment",
+            icon: MessageSquarePlusIcon,
+            onClick: () => console.log("TODO: Comment"),
+            isActive: false
             
-        }
+        },
+        {
+            label: "List Todo",
+            icon: ListTodoIcon,
+            onClick: () => editor?.chain().focus().toggleTaskList().run(),
+            isActive: editor?.isActive("taskList"),
+
+            
+        },
+        {
+            label: "Remove Formatting",
+            icon: RemoveFormattingIcon,
+            onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+
+            
+        },
     ]
   ];
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
       {sections[0].map((item)=>(
+        <ToolbarButton key={item.label}{...item}/>
+      ))}
+      <Separator className="h-6 w-px bg-neutral-300"/>
+      {/*TODO: Font family */}
+      <Separator className="h-6 w-px bg-neutral-300"/>
+      {/*TODO: Heading*/}
+      
+      {/*TODO: Font size */}
+      {sections[1].map((item)=>(
+        <ToolbarButton key={item.label}{...item}/>
+      ))}
+    {/* TODO TEXT COLOR*/}
+            {/* TODO HIGHLIGHT COLOR*/}
+            <Separator className="h-6 w-px bg-neutral-300"/>
+            {/* TODO Link*/}
+            {/* TODO Image*/}
+            {/* TODO LineHeight*/}
+            {/* TODO List*/}
+      {sections[2].map((item)=>(
         <ToolbarButton key={item.label}{...item}/>
       ))}
     </div>
